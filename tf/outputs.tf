995c551e70f1a61a60985b0f51bf2faa34b3d419
Nerea MR <nerearojaas@gmail.com>
2025-01-18 23:51:44 +0100

    outputs.tf

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,9 +1,9 @@
 
 # + Outputs
 output "public_ip" {
-  value = aws_instance.web_server.public_ip
+  value = aws_instance.mi_instancia.public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.web_server.id
+  value = aws_instance.mi_instancia.id
 }
