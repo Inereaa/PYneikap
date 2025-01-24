c4feb927335eb2bbe5d44db25c10cd9d4e6ee0ad
Nerea MR <nerearojaas@gmail.com>
2025-01-24 19:02:44 +0100

    quitados .cert del start.sh

--- tf/start.sh
+++ tf/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iniciar JSON Server con HTTPS
-json-server --watch /usr/local/apache2/htdocs/db/dbProductos.json --port 3000 --cert /usr/local/apache2/conf/certificate.crt --key /usr/local/apache2/conf/private.key &
+json-server --watch /usr/local/apache2/htdocs/db/dbProductos.json --port 3000 &
 
 # Iniciar Apache en primer plano
 httpd-foreground
